6d76c0ce2a05ada2ac90038fc0682a514a263d9f
nasah <nasahemon@gmail.com>
2021-12-08 21:28:47 +0600

    update account holder using jdbc template

--- src/main/java/com/example/BankAccounts/AccountHolderInfo/Adapter/out/persistance/Adapter/AccountHolderPersistenceAdapter.java
+++ src/main/java/com/example/BankAccounts/AccountHolderInfo/Adapter/out/persistance/Adapter/AccountHolderPersistenceAdapter.java
@@ -1,6 +1,7 @@
 package com.example.BankAccounts.AccountHolderInfo.Adapter.out.persistance.Adapter;
 
 import com.example.BankAccounts.AccountHolderInfo.Adapter.out.persistance.Entity.AccountHolderDbEntity;
+import com.example.BankAccounts.AccountHolderInfo.Adapter.out.persistance.Entity.AccountTypeDbEntity;
 import com.example.BankAccounts.AccountHolderInfo.Adapter.out.persistance.Repository.AccountHolderRepository;
 import com.example.BankAccounts.AccountHolderInfo.Application.port.out.AccountHolderDetailsPort;
 import com.example.BankAccounts.AccountHolderInfo.Domain.AccountHolderEntity;
@@ -8,6 +9,7 @@ import com.example.BankAccounts.MyUtil.CustomAccountMapper;
 import com.example.BankAccounts.MyUtil.MyException;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -57,6 +59,54 @@ public class AccountHolderPersistenceAdapter implements AccountHolderDetailsPort
         return ListOfAccountHolder;
     }
 
+
+
+    @Override
+    public String deleteAccountHolderDetailsById(Long id) throws MyException {
+        String message="ok";
+        try{
+            jdbcTemplate.update("DELETE FROM public.account_holder_info WHERE account_id=?", id);
+        }catch(Exception e){
+            e.printStackTrace();
+            throw new MyException(HttpStatus.NOT_FOUND,
+                    "Adapter: Doesn't have anything with id : " + id);
+        }
+        return message;
+    }
+
+    @Override
+    public AccountHolderEntity updateAccountHolderDetailsById(Long id, AccountHolderEntity accountHolderEntity) throws MyException {
+        try{
+            AccountHolderDbEntity updatedAccountHolderDetails = mapper.map(accountHolderEntity,AccountHolderDbEntity.class);
+            jdbcTemplate.update("UPDATE public.account_holder_info SET address=?,age=?,contact_no=?,created_on=?,"+
+                            "date_of_birth=?,gender=?,hobby=?,\"name\"=?,id=? WHERE account_id=?",
+                    updatedAccountHolderDetails.getAddress(),
+                    updatedAccountHolderDetails.getAge(),
+                    updatedAccountHolderDetails.getContactNo(),
+                    updatedAccountHolderDetails.getCreatedOn(),
+                    updatedAccountHolderDetails.getDateOfBirth(),
+                    updatedAccountHolderDetails.getGender(),
+                    updatedAccountHolderDetails.getHobby(),
+                    updatedAccountHolderDetails.getName(),
+                    updatedAccountHolderDetails.getAccountType().getId(),
+                    id);
+            String sql = "SELECT a.account_id ,a.address ,a.age ,a.contact_no ,a.created_on ,a.date_of_birth ,a.gender ,a.hobby ,a.\"name\" ,a.id ,t.\"type\" \n" +
+                    "FROM public.account_holder_info a\n" +
+                    "INNER join public.account_types t \n" +
+                    "on a.id = t.id WHERE account_id=? ;";
+            AccountHolderDbEntity savedAccountHolderEntity= jdbcTemplate.queryForObject(sql,
+                    new CustomAccountMapper(),id);
+            return mapper.map(savedAccountHolderEntity,AccountHolderEntity.class);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            throw new MyException(HttpStatus.NOT_FOUND,
+                    "Adapter: repository doesn't have anything with id : " + id);
+        }
+    }
+
+
+}
 //    @Override
 //    public List<AccountHolderEntity> fetchAllAccountHolderDetails() throws MyException {
 //        List<AccountHolderEntity> ListOfAccountHolder;
@@ -75,41 +125,17 @@ public class AccountHolderPersistenceAdapter implements AccountHolderDetailsPort
 //        return ListOfAccountHolder;
 //    }
 
-    @Override
-    public String deleteAccountHolderDetailsById(Long id) throws MyException {
-        String message="ok";
-        try{
-            accountHolderRepository.deleteById(id);
-            if(accountHolderRepository.existsById(id)) return message="Still_Exist";
-        }catch(IllegalArgumentException e){
-            throw new MyException(HttpStatus.NOT_FOUND,
-                    "Adapter: Doesn't have anything with id : " + id);
-        }
-        return message;
-    }
-
-    @Override
-    public AccountHolderEntity updateAccountHolderDetailsById(Long id, AccountHolderEntity accountHolderEntity) throws MyException {
-        try{
-            AccountHolderDbEntity found = accountHolderRepository.findById(id).get();
-            AccountHolderDbEntity updatedAccountHolderDetails = mapper.map(accountHolderEntity,AccountHolderDbEntity.class);
-            updatedAccountHolderDetails.setId(id);
-            AccountHolderDbEntity savedAccountHolderEntity= accountHolderRepository.save(updatedAccountHolderDetails);
-            return mapper.map(savedAccountHolderEntity,AccountHolderEntity.class);
-        }
-        catch(Exception e){
-            throw new MyException(HttpStatus.NOT_FOUND,
-                    "Adapter: repository doesn't have anything with id : " + id);
-        }
-    }
-
-
-    public List<AccountHolderDbEntity> getAllAccountHolder(){
-        String sql = "SELECT a.account_id ,a.address ,a.age ,a.contact_no ,a.created_on ,a.date_of_birth ,a.gender ,a.hobby ,a.\"name\" ,a.id ,t.\"type\" \n" +
-                "FROM public.account_holder_info a\n" +
-                "INNER join public.account_types t \n" +
-                "on a.id = t.id ;";
-        List<AccountHolderDbEntity> AccountHolders = jdbcTemplate.query(sql, new CustomAccountMapper());
-        return AccountHolders;
-    }
-}
+//    @Override
+//    public AccountHolderEntity updateAccountHolderDetailsById(Long id, AccountHolderEntity accountHolderEntity) throws MyException {
+//        try{
+//            AccountHolderDbEntity found = accountHolderRepository.findById(id).get();
+//            AccountHolderDbEntity updatedAccountHolderDetails = mapper.map(accountHolderEntity,AccountHolderDbEntity.class);
+//            updatedAccountHolderDetails.setId(id);
+//            AccountHolderDbEntity savedAccountHolderEntity= accountHolderRepository.save(updatedAccountHolderDetails);
+//            return mapper.map(savedAccountHolderEntity,AccountHolderEntity.class);
+//        }
+//        catch(Exception e){
+//            throw new MyException(HttpStatus.NOT_FOUND,
+//                    "Adapter: repository doesn't have anything with id : " + id);
+//        }
+//    }
